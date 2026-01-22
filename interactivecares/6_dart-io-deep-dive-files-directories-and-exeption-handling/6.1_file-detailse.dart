b1e386f55d9a2776e3034ee3bfdc72c5fe28b620
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-01-23 02:29:53 +0600

    adde: create directory

--- interactivecares/6_dart-io-deep-dive-files-directories-and-exeption-handling/6.1_file-detailse.dart
+++ interactivecares/6_dart-io-deep-dive-files-directories-and-exeption-handling/6.1_file-detailse.dart
@@ -6,15 +6,16 @@
 import 'dart:io';
 
 void main(){
-  // File tempFile = File("data.txt");
-  // tempFile.writeAsString("Hello i am");
-  //  File file = File("data.txt");
-  //  String content = file.readAsStringSync();
-  //  print(content);
+  File tempFile = File("data.txt");
+  tempFile.writeAsString("Hello i am");
+  tempFile.writeAsString("\nwhat is your name", mode: FileMode.append);
+   File file = File("data.txt");
+   String content = file.readAsStringSync();
+   print(content);
 
-  File file = File("lib/dart_practice.dart");
-  String content = file.readAsStringSync();
-  print(content);
+  // File file = File("lib/dart_practice.dart");
+  // String content = file.readAsStringSync();
+  // print(content);
 
 
 }
---
+++ interactivecares/6_dart-io-deep-dive-files-directories-and-exeption-handling/6.4_create-directory.dart
@@ -0,0 +1,13 @@
+
+
+
+
+
+import 'dart:io';
+
+void main(){
+
+  Directory dir = Directory("img/png");
+  dir.createSync();
+
+}
